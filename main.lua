82c0a552ea40fcbbd5f089640b7c827ccf7e7557
Textmode <netsdanita@hotmail.com>
2016-07-09 17:51:19 +1000

    extracted player update code to function (prep for entities)

--- main.lua
+++ main.lua
@@ -12,27 +12,24 @@ images = {}
 -- Bullet storage
 bullets = {} -- array of current bullets being drawn and updated
 
+local player_upd
+
 function love.load(arg)
 	local lg = love.graphics
 	images.bullet = lg.newImage('assets/bullet.png')
 	images.player = lg.newImage('assets/fighter.png') -- CC0 licensed from opengameart
 	
-	player = { x = 200, y = 710, speed = 150, img = images.player }
+	player = { x = 200, y = 710, speed = 150, img = images.player, update = player_upd}
 end
 
-function love.update(dt)
-  -- Quitting
-  if love.keyboard.isDown('escape') then
-    love.event.quit()
-  end
-
+function player_upd(self, dt)
   if love.keyboard.isDown('left', 'a') then
-    if player.x > 0 then -- binds player to the map
-      player.x = player.x - (player.speed*dt)
+    if self.x > 0 then -- binds player to the map
+      self.x = self.x - (self.speed*dt)
     end
   elseif love.keyboard.isDown('right', 'd') then
-    if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
-      player.x = player.x + (player.speed*dt)
+    if self.x < (love.graphics.getWidth() - self.img:getWidth()) then
+      self.x = self.x + (self.speed*dt)
     end
   end
 
@@ -45,11 +42,21 @@ function love.update(dt)
   -- Firing
   if love.keyboard.isDown('space', ' ', 'rctrl', 'lctrl', 'ctrl') and canShoot then
     -- Create some bullets
-    newBullet = { x = player.x + (images.player:getWidth()/2), y = player.y, img = images.bullet }
+    newBullet = { x = self.x + (self.img:getWidth()/2), y = self.y, img = images.bullet }
     table.insert(bullets, newBullet)
     canShoot = false
     canShootTimer = canShootTimerMax
   end
+end
+
+
+function love.update(dt)
+  -- Quitting
+  if love.keyboard.isDown('escape') then
+    love.event.quit()
+  end
+  
+  if player.update then player:update(dt) end
 
   -- update bullet positions
   for i, bullet in ipairs(bullets) do
